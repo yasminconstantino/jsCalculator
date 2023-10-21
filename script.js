b9e809dd44af15dac9cfb026bfb4ba10e034fa5b
Yasmin Constantino <112824120+yasminconstantino@users.noreply.github.com>
2023-10-21 15:10:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
- // Passo a passo das funcionalidades da calculadorinha:
- // pegar o elemento do visor
- var visor = document.getElementById("visor");
+// pegar o elemento do visor
+var visor = document.getElementById("visor");
 
- // quando uma tecla for pressionada, seu valor vai pro visor
- function adicionae(tecla) {
-   visor.value = visor.value + tecla;
- }
+// quando uma tecla for pressionada, seu valor vai pro visor
+function adicionae(tecla) {
+  visor.value = visor.value + tecla;
+}
 
  // quando o "c" for pressionado limpa o visor
- function limpezatotal() {
-   visor.value = "";
-   alert("Apagando... ✨");
- }
+function limpezatotal() {
+  visor.value = "";
+  alert("Apagando... ✨");
+}
 
  // quando o "=" for pressionado calcula
- function calculari() {
-   visor.value = eval(visor.value);
- }
+function calculari() {
+  visor.value = eval(visor.value);
+}
+
+// Adicione um evento para teclas pressionadas
+document.addEventListener('keydown', function(event) {
+  // Verifica se a tecla pressionada é um número ou operador
+  if (/[\d\+\-\*\/]/.test(event.key)) {
+    adicionae(event.key);
+  } else if (event.key === 'Enter') {
+    event.preventDefault();
+    calculari();
+  } else if (event.key === 'c' || event.key === 'C') {
+    limpezatotal();
+  }
+}); 
